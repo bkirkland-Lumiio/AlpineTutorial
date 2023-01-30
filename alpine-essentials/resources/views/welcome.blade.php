142e1b6c37ee540a7672b8da9f1ec7551d04b5ed
Braiden Kirkland <braiden.kirkland@lumiio.com>
2023-01-30 13:11:46 -0800

    Video 6 - Transitions 101

--- alpine-essentials/resources/views/welcome.blade.php
+++ alpine-essentials/resources/views/welcome.blade.php
@@ -8,28 +8,22 @@
         <title>Alpine Examples</title>
     </head>
 
-    <body class="p-10 max-w-lg mx-auto">
-        <div
-            class="bg-gray-300 px-10 py-6 rounded"
-            x-data="taskApp()"
-        >
-            <form @submit.prevent="submit">
-                <input
-                    type="text"
-                    placeholder="Go to the market..."
-                    x-model="newTask"
-                    class="w-full px-1"
-                >
-            </form>
+    <body class="grid items-center justify-center h-screen">
+        <div x-data="{ show: true }">
+            <div class="w-12 h-12">
+                <div
+                        class="bg-green-400 w-full h-full"
+                        x-show="show"
+                        x-transition:enter="transition transform duration-1000"
+                        x-transition:enter-start="opacity-0 scale-125"
+                        x-transition:enter-end="opacity-100 scale-100"
+                        x-transition:leave="transition duration-200"
+                        x-transition:leave-start="opacity-100"
+                        x-transition:leave-end="opacity-0"
+                ></div>
+            </div>
 
-            <ul class="list-disc mt-3">
-                <template x-for="(task, index) in tasks" :key="index">
-                    <li>
-                        <input type="checkbox" x-model="task.completed">
-                        <span x-text="task.body" :class="{ 'line-through' : task.completed }"></span>
-                    </li>
-                </template>
-            </ul>
+            <button @click="show = ! show">Toggle</button>
         </div>
     </body>
 </html>
